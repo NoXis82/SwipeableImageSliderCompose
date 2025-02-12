6e53064a67f6c25fb0dc551d27d4a8fdcc83458a
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-12 20:28:46 +0300

    Define CustomSlider Composable function

---
+++ app/src/main/java/ru/noxis/swipeableimageslider/CustomSlider.kt
@@ -0,0 +1,96 @@
+package ru.noxis.swipeableimageslider
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Scale
+import kotlin.math.absoluteValue
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CustomSlider(
+    modifier: Modifier = Modifier,
+    sliderList: MutableList<String>,
+    backwardIcon: ImageVector = Icons.AutoMirrored.Default.KeyboardArrowLeft,
+    forwardIcon: ImageVector = Icons.AutoMirrored.Default.KeyboardArrowRight,
+    dotsActiveColor: Color = Color.DarkGray,
+    dotsInActiveColor: Color = Color.LightGray,
+    dotsSize: Dp = 10.dp,
+    pagerPaddingValues: PaddingValues = PaddingValues(horizontal = 65.dp),
+    imageCornerRadius: Dp = 16.dp,
+    imageHeight: Dp = 250.dp,
+) {
+
+    val pagerState = rememberPagerState { sliderList.size }
+    val scope = rememberCoroutineScope()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                contentPadding = pagerPaddingValues,
+                modifier = modifier.weight(1f)
+            ) { page ->
+                val pageOffset =
+                    (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
+
+                val scaleFactor = 0.75f + (1f - 0.75f) * (1f - pageOffset.absoluteValue)
+
+                Box(
+                    modifier = modifier.graphicsLayer {
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+                    }
+                    .alpha(scaleFactor.coerceIn(0f, 1f))
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(imageCornerRadius))
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current).scale(Scale.FILL)
+                            .crossfade(true).data(sliderList[page]).build(),
+                        contentDescription = "Image",
+                        contentScale = ContentScale.Crop,
+                       // placeholder = painterResource(id = R.drawable.img),
+                        modifier = modifier.height(imageHeight)
+//                            .alpha(if (pagerState.currentPage == page) 1f else 0.5f)
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/ru/noxis/swipeableimageslider/MainActivity.kt
+++ app/src/main/java/ru/noxis/swipeableimageslider/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.noxis.swipeableimageslider.ui.theme.SwipeableImageSliderTheme
@@ -20,28 +21,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             SwipeableImageSliderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val sliderList = remember {
+                        mutableListOf(
+                            "https://www.gstatic.com/webp/gallery/1.webp",
+                            "https://www.gstatic.com/webp/gallery/2.webp",
+                            "https://www.gstatic.com/webp/gallery/3.webp",
+                            "https://www.gstatic.com/webp/gallery/4.webp",
+                            "https://www.gstatic.com/webp/gallery/5.webp",
+                        )
+                    }
+                    CustomSlider(modifier = Modifier.padding(innerPadding),sliderList)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SwipeableImageSliderTheme {
-        Greeting("Android")
     }
 }
